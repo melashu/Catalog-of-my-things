af22186930c298044e4da1c67d0af9ed479d751e
Bantealem <bantem2021@gmail.com>
2022-12-01 14:04:19 +0300

    fix linters

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -3,9 +3,9 @@ require_relative '../lib/label'
 require 'date'
 
 describe Book do
-    before (:all) do
-        @book1 = Book.new('New publisher', 'bad', Date.parse('2001-02-03'))
-    end
+  before(:all) do
+    @book1 = Book.new('New publisher', 'bad', Date.parse('2001-02-03'))
+  end
   context 'when testing the book class' do
     it 'should create a new book when book class instantiated' do
       book = Book.new('New publisher', 'Good', '2022-09-09')
@@ -14,9 +14,8 @@ describe Book do
       expect(book.publish_date).to eq '2022-09-09'
     end
   end
-  
-    it 'can_be_archived? should return false' do
-      expect(@book1.can_be_archived?).to be true
-    end
-    
+
+  it 'can_be_archived? should return false' do
+    expect(@book1.can_be_archived?).to be true
+  end
 end
