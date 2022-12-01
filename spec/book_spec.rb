86d0327172b9a6ddcd1a061e1ab68578e313ab34
Bantealem <bantem2021@gmail.com>
2022-12-01 14:00:46 +0300

    Add unit test for can be achived method

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,7 +1,11 @@
 require_relative '../lib/book'
+require_relative '../lib/label'
 require 'date'
 
 describe Book do
+    before (:all) do
+        @book1 = Book.new('New publisher', 'bad', Date.parse('2001-02-03'))
+    end
   context 'when testing the book class' do
     it 'should create a new book when book class instantiated' do
       book = Book.new('New publisher', 'Good', '2022-09-09')
@@ -10,4 +14,9 @@ describe Book do
       expect(book.publish_date).to eq '2022-09-09'
     end
   end
+  
+    it 'can_be_archived? should return false' do
+      expect(@book1.can_be_archived?).to be true
+    end
+    
 end
